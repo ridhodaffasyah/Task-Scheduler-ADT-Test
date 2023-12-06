2739b42b949e81aee951c1c1577608239fabbaf5
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-12-06 22:53:43 +0700

    feat: add components atom button

--- components/atom/Button/index.tsx
+++ components/atom/Button/index.tsx
@@ -3,9 +3,9 @@ import { ButtonProps } from "@/utils/interface";
 
 const Button: React.FC<ButtonProps> = ({ isExplore, text, handleButton }) => {
   return isExplore ? (
-    <button className="bg-black text-white px-[1.5rem] py-[0.75rem] rounded-[0.25rem] border-none text-[1rem] font-bold cursor-pointer transition-all duration-[0.25s] ease-in-out hover:bg-orange-300 sm:hidden md:flex" onClick={handleButton}>{text}</button>
+    <button className="bg-black text-white px-[1.5rem] py-[0.75rem] rounded-[0.25rem] border-none text-[1rem] font-bold cursor-pointer transition-all duration-[0.25s] ease-in-out hover:bg-[#DFA46D] sm:hidden md:flex" onClick={handleButton}>{text}</button>
   ) : (
-    <button className="bg-orange-300 text-white px-[0.75rem] py-[1.5rem] rounded-[0.25rem] border-none text-[1rem] font-bold cursor-pointer transition-all duration-[0.25s] ease-in-out hover:bg-orange-300 sm:hidden md:flex" onClick={handleButton}>{text}</button>
+    <button className="bg-[#DFA46D] text-white px-[0.75rem] py-[1.5rem] rounded-[0.25rem] border-none text-[1rem] font-bold cursor-pointer transition-all duration-[0.25s] ease-in-out hover:bg-[#DFA46D] sm:hidden md:flex" onClick={handleButton}>{text}</button>
   );
 };
 
