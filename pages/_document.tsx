4fa4be184fdde8a73a861069ce9021e3349fd488
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2023-12-06 22:54:13 +0700

    feat: change favicon and add title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/images/fav-logo.png" />
+        <title>Task Scheduler</title>
       <body>
         <Main />
         <NextScript />
